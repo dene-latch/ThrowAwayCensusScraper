3d8c11cab3257111d5961a980bf45a34799d9b39
dene farrell <dene.farrell@gmail.com>
2019-10-26 19:50:38 -0400

    basic postgres stuff

--- Makefile
+++ Makefile
@@ -7,5 +7,13 @@ appname="throwawaycensusscraper"
 run:
 	@docker-compose run scraper /bin/bash -c "./target/universal/stage/bin/${appname}"
 
+dc-clean:
+	docker-compose down && docker-compose rm -v
+
+pg-test:
+	docker exec -it my_postgres  psql -U john -d mydb -c "CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);"
+
+pg-loc:
+	PGPASSWORD=pwd0123456789  psql -h localhost -p 54320 -U john -d mydb
 
  .PHONY: run
---
+++ src/main/scala/PGLoader.scala
@@ -0,0 +1,30 @@
+import java.sql.DriverManager
+import java.sql.Connection
+
+object PGManager {
+
+  def main(args: Array[String]): Unit = {
+
+    val driver = "org.postgresql.Driver"
+    val url = "jdbc:postgresql://0.0.0.0:5432/mydb"
+    val username = "john"
+    val password = "pwd0123456789"
+
+    var connection:Connection = null
+
+    try {
+      Class.forName(driver)
+      connection = DriverManager.getConnection(url, username, password)
+//      connection = Database.forUrl(url, username, password)
+
+      val statement = connection.createStatement()
+      statement.executeQuery("CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);")
+      statement.executeQuery("INSERT INTO test (num, data) VALUES (%s, %s), (100, 'abcdef')" )
+      val queryResult = statement.executeQuery("SELECT * FROM test;")
+    } catch {
+      case e => e.printStackTrace
+    }
+    connection.close()
+  }
+
+}
